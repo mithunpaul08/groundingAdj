a229d584c7da3f9274f53fbc1b9209aa9cae0692
mithun <mithunpaul08@gmail.com>
2017-11-04 18:37:37 -0700

    added code to compare with glove

--- src/main/scala/initializer.scala
+++ src/main/scala/initializer.scala
@@ -1,4 +1,4 @@
-package initializer
+package adjVectorCompare
 
 import java.io.{BufferedWriter, File, FileWriter}
 
@@ -6,8 +6,14 @@ import org.clulab.embeddings.word2vec.Word2Vec
 import utils._
 
 object initializer extends App {
+  val w2v = "//data/nlp/corpora/word2vec/gigaword/vectors.txt";
+  val glove = "//data/nlp/corpora/glove/6B/glove.6B.200d.txt"
+
+ val outputFileW2v="comparew2vsim.txt"
+  val outputFileGlove="turkGlove.txt"
 
 
+  //for each adj in the turk task adjectives get and sort by its intercept
   val data = io.Source.fromFile("src/main/resources/turk_fullText.txt").getLines().toArray
   val split_data = data.tail.map { e =>
     val fields = e.split("\t")
@@ -16,45 +22,9 @@ object initializer extends App {
     (adj, intercept)
   }
   val sorted_split_data = split_data.sortBy(_._2)
-
-  writeToFile(sorted_split_data.mkString("\n"),"Turksim.txt","src/main/outputs/")
-
-
-  val objW2v = Word2Vec;
-  val w2v = new Word2Vec("//data/nlp/corpora/word2vec/gigaword/vectors.txt")
-
-  val firstAdj= sorted_split_data(0)._1
-
-//  for ((adj, intercept) <- sorted_split_data) {
-//    val adj_sanitized = objW2v.sanitizeWord(adj);
-//    print(adj_sanitized)
-//    val sim=w2v.similarity(firstAdj,adj_sanitized);
-//
-//  }
-
+  writeToFile(sorted_split_data.mkString("\n"), "Turksim.txt", "src/main/outputs/")
+  //pick the adjective with the smallest intercept
+  val firstAdj = sorted_split_data(0)._1
   println(sorted_split_data.mkString("\n"))
-
-
-
-  val w2vSim=sorted_split_data.map { d=>
-
-    val adj= d._1;
-    val intercept=d._2
-    val adj_sanitized = objW2v.sanitizeWord(adj);
-    val firstAdj_sanitized = objW2v.sanitizeWord(firstAdj);
-    print(adj_sanitized)
-    val sim=w2v.similarity(firstAdj_sanitized,adj_sanitized);
-    (adj_sanitized,sim)
-
-  }
-
-
-val sorted_w2vSim=w2vSim.sortBy(-_._2)
-  println(sorted_w2vSim.mkString("\n"))
-
-  writeToFile(sorted_w2vSim.mkString("\n"),"comparew2vsim.txt","src/main/outputs/")
-
-
-
-
+  calculateSimilarity(glove,firstAdj,outputFileGlove)
 }
