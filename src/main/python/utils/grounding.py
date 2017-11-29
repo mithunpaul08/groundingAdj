5026e0305def11c5ddea96f0f94a14627dd7d04e
Mithun Paul <mithunpaul@chung.cs.arizona.edu>
2017-11-29 16:11:35 -0700

    creates one hot for one adjective in loop

--- src/main/python/utils/grounding.py
+++ src/main/python/utils/grounding.py
@@ -61,7 +61,7 @@ def with_one_hot_adj(cwd,turkFile):
                 uniq_adj[a]=counter
                 counter=counter+1
 
-        print("total number of unique adjectives is "+len(uniq_adj))
+
    
         uniq_turker={}
         turk_counter=1
@@ -72,19 +72,25 @@ def with_one_hot_adj(cwd,turkFile):
                 uniq_turker[b]=turk_counter
                 turk_counter=turk_counter+1
 
+        uniq_adj_count=len(uniq_adj)
+        uniq_turker_count=len(uniq_turker)
+
+
         print("total number of unique adjectives is "+str(len(uniq_adj)))
-        print("total number of unique adjectives is "+str(len(uniq_turker)))
-        sys.exit(1)
+        print("total number of unique turkers is "+str(len(uniq_turker)))
+
+
+
+
+        #Split data in to train-dev-test
+
         noOfRows=df_raw_turk_data.shape[0]
-
-
 
         #create an numpy array of that range
         allIndex=np.arange(noOfRows)
 
         #now shuffle it and split
         np.random.shuffle(allIndex)
-
         splitTurk=np.array_split(allIndex,2)
 
         #print(df_raw_turk_data["logrespdev"][0])
@@ -97,12 +103,7 @@ def with_one_hot_adj(cwd,turkFile):
         dev=dev_test[0]
         test=dev_test[1]
 
-        #print(trainingData.shape)
-        #print(dev.shape)
-        #print(test.shape)
 
-        #print(trainingData["mean"])
-        cbow4 = "glove_vectors_syn_ant_sameord_difford.txt"
 
 
         # #for each of the adjective create a one hot vector
@@ -110,3 +111,19 @@ def with_one_hot_adj(cwd,turkFile):
             #give this index to the actual data frame
             adj=df_raw_turk_data["adjective"][eachTurkRow]
 
+            #get the index of the adjective
+            adjIndex=uniq_adj[adj]
+            print("adjIndex:"+str(adjIndex))
+            print("uniq_adj_count:"+str(uniq_adj_count))
+
+            #create a one hot vector for all adjectives
+            one_hot=np.zeros(uniq_adj_count)
+            print(one_hot)
+            print("one hot shape:"+str((one_hot.shape)))
+            one_hot[adjIndex]=1
+
+            print(one_hot)
+
+
+
+            sys.exit(1)
