294912b736bc28adf37bc229e10dca7fbc9cd4a8
Mithun Paul <mithunpaul@chung.cs.arizona.edu>
2017-11-29 16:29:16 -0700

    added one hot code with_one_hot_adj

--- src/main/python/utils/grounding.py
+++ src/main/python/utils/grounding.py
@@ -96,3 +96,101 @@ def predict_grounding(cwd,turkFile):
 
 
 
+
+def with_one_hot_adj(cwd,turkFile):
+        df_raw_turk_data=readRawTurkDataFile(cwd, turkFile)
+        print(df_raw_turk_data["adjective"][0])
+
+        #create a hash table to store unique adj
+        uniq_adj={}
+        counter=1
+
+        #create a total list of unique adj in this collection
+        for a in df_raw_turk_data["adjective"]:
+
+            if(a) not in uniq_adj:
+                #if its not there already add it as the latest element
+                uniq_adj[a]=counter
+                counter=counter+1
+
+
+
+        uniq_turker={}
+        turk_counter=1
+        #create a total list of unique turkers in this collection
+        for b in df_raw_turk_data["turker"]:
+            if(b) not in uniq_turker:
+                #if its not there already add it as the latest element
+                uniq_turker[b]=turk_counter
+                turk_counter=turk_counter+1
+
+        uniq_adj_count=len(uniq_adj)
+        uniq_turker_count=len(uniq_turker)
+
+
+        print("total number of unique adjectives is "+str(len(uniq_adj)))
+        print("total number of unique turkers is "+str(len(uniq_turker)))
+
+
+
+
+        #Split data in to train-dev-test
+
+        noOfRows=df_raw_turk_data.shape[0]
+
+        #create an numpy array of that range
+        allIndex=np.arange(noOfRows)
+
+        #now shuffle it and split
+        np.random.shuffle(allIndex)
+        splitTurk=np.array_split(allIndex,2)
+
+        #print(df_raw_turk_data["logrespdev"][0])
+
+        trainingData=splitTurk[0]
+        rest=splitTurk[1]
+
+        #split the rest into half as dev and test
+        dev_test=np.array_split(rest,2)
+        dev=dev_test[0]
+        test=dev_test[1]
+
+
+
+
+        # #for each of the adjective create a one hot vector
+        for eachTurkRow in trainingData:
+
+            ########create a one hot vector for adjective
+            # give this index to the actual data frame
+            adj=df_raw_turk_data["adjective"][eachTurkRow]
+
+            #get the index of the adjective
+            adjIndex=uniq_adj[adj]
+            #print("adjIndex:"+str(adjIndex))
+            #print("uniq_adj_count:"+str(uniq_adj_count))
+
+            #create a one hot vector for all adjectives
+            one_hot=np.zeros(uniq_adj_count)
+            #print(one_hot)
+            #print("one hot shape:"+str((one_hot.shape)))
+            one_hot[adjIndex]=1
+
+            ################to create a one hot vector for turker data also
+            #get the id number of of the turker
+            turkerId=df_raw_turk_data["turker"][eachTurkRow]
+            turkerIndex=uniq_turker[turkerId]
+            print("turkerIndex:"+str(turkerIndex))
+
+            #create a one hot vector for all turkers
+            one_hotT=np.zeros(uniq_turker_count)
+            #print(one_hotT)
+            #print("one one_hotT shape:"+str((one_hotT.shape)))
+            one_hotT[turkerIndex]=1
+            print(one_hotT)
+
+
+            ################get the mean and variance for this row and attach to this one hot
+
+
+            sys.exit(1)
