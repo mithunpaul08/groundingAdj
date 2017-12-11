f9421b4e5287c1845a6ab9f6dc0895406cfa80cf
Mithun Paul <mithunpaul@chung.cs.arizona.edu>
2017-12-10 23:18:14 -0700

    added while true

--- src/main/python/initializer.py
+++ src/main/python/initializer.py
@@ -30,64 +30,70 @@ if __name__ == "__main__":
     try:
 
 
-
-
-
         features, y, adj_lexicon,all_adj= get_features_y(cwd, turkFile,False)
+        while True:
+                print("                      ")
+                print("          ******            ")
 
-        print(features.shape)
+                print("Welcome to Grounding FOr Adjectives Tagger. Please pick one of the following:")
 
-        #get the 300 embedding vector from glove for an adj
-        run_adj_emb(features,y,adj_lexicon,all_adj)
-
-
-        adj_lexicon_flipped = dict()
-        #total number of unique adjectives
-        num_adj = len(adj_lexicon)
-
-        #key=index value=adjective
-        for a, idx in adj_lexicon.items():
-            adj_lexicon_flipped[idx] = a
-
-        #actual linear regression part- how much weight should it assigne to each of 1-hot-adj-vector, mean and variance
-
-        #will be of size 1x100=98 adj, one mean and variance
-        learned_weights = runLR(features, y)
-
-        print(str(learned_weights.shape))
-        #sys.exit(1)
-        #print("NumUniqueAdj: ", num_adj)
-        # Get the weights that correspond to the individual adjs
-        adj_intercepts_learned = learned_weights[:num_adj]
-        #pairing weights with adjectives.
-        adj_pairs = [(learned_weights[0][i], adj_lexicon_flipped[i]) for i in range(num_adj)]
-
-        #print(adj_pairs[:2])
-
-        #sorting them by their weight
-        sorted_adjs = sorted(adj_pairs, key=lambda x: x[0], reverse=True)
-
-        #print highest 20 intercepts and lowest 20 intercepts
-        print(sorted_adjs[:20])
-        print(sorted_adjs[-20:])
+                print("To run the model with just linear regression Press:1")
+                print("To run the model with a dense NN, embeddings through linear regression Press:2")
+                print("To exit Press:0")
 
 
-        #get the actual intercepts
-        # adj_intercepts_original=readWithSpace(cwd,turkInterceptFile)
-        # print((adj_intercepts_original["adjective"][0]))
-        # # print((adj_intercepts_original["intercept"][0]))
-        # print(str(adj_intercepts_original.shape))
-        # print(str(adj_intercepts_learned.shape))
-        #sys.exit(1)
+                myInput=input("what is your choice:")
+                if(myInput=="2"):
+
+                    #get the 300 embedding vector from glove for an adj
+                    run_adj_emb(features,y,adj_lexicon,all_adj)
+
+                else:
+                    if(myInput=="0"):
+                        print("******Good Bye")
+                        break;
+                    else:
+                        if(myInput=="1"):
+
+    
 
 
 
 
-        # features,y =predict_grounding(cwd,turkFile)
-        # print("size of features is:")
-        # print((features.shape))
-        # print("size of y is:")
-        # print((y.shape))
+                            print(features.shape)
+
+
+
+
+                            adj_lexicon_flipped = dict()
+                            #total number of unique adjectives
+                            num_adj = len(adj_lexicon)
+
+                            #key=index value=adjective
+                            for a, idx in adj_lexicon.items():
+                                adj_lexicon_flipped[idx] = a
+
+                            #actual linear regression part- how much weight should it assigne to each of 1-hot-adj-vector, mean and variance
+
+                            #will be of size 1x100=98 adj, one mean and variance
+                            learned_weights = runLR(features, y)
+
+                            print(str(learned_weights.shape))
+                            #sys.exit(1)
+                            #print("NumUniqueAdj: ", num_adj)
+                            # Get the weights that correspond to the individual adjs
+                            adj_intercepts_learned = learned_weights[:num_adj]
+                            #pairing weights with adjectives.
+                            adj_pairs = [(learned_weights[0][i], adj_lexicon_flipped[i]) for i in range(num_adj)]
+
+                            #print(adj_pairs[:2])
+
+                            #sorting them by their weight
+                            sorted_adjs = sorted(adj_pairs, key=lambda x: x[0], reverse=True)
+
+                            #print highest 20 intercepts and lowest 20 intercepts
+                            print(sorted_adjs[:20])
+                            print(sorted_adjs[-20:])
 
     ##################################end of dev phase####################
     except:
