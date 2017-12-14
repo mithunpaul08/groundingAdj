7f2bc3cc0b9eb62edbb8b2231220a81220af6939
mithun <mithunpaul08@gmail.com>
2017-12-13 17:23:36 -0700

    epochs=30

--- src/main/python/utils/squish.py
+++ src/main/python/utils/squish.py
@@ -16,7 +16,7 @@ import numpy as np
 torch.manual_seed(1)
 
 dense_size=10
-noOfEpochs=5
+noOfEpochs=30
 class AdjEmb(nn.Module):
     #the constructor. Pass whatever you need to
     def __init__(self,turkCount):
