80987a4482fc5f7e574fd869e5b988c5890baba8
mithun <mithunpaul08@gmail.com>
2017-12-28 15:18:24 -0700

    replaced tanh with relu

--- src/main/python/utils/squish.py
+++ src/main/python/utils/squish.py
@@ -67,7 +67,8 @@ class AdjEmb(nn.Module):
         embV=Variable(emb,requires_grad=False)
 
         #give that to the squishing layer
-        squished_layer=F.tanh(self.squish(embV))
+        #squished_layer=F.tanh(self.squish(embV))
+        squished_layer = F.relu(self.squish(embV))
 
         feature_squished = torch.cat((feats, squished_layer))  # .data))
         return self.fc(feature_squished)
