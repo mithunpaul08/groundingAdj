c52a647f59df0d8bdcb85ad889baf3cf62f770a3
Mithun Paul <mithunpaul@chung.cs.arizona.edu>
2018-01-11 15:40:48 -0700

    all adj

--- src/main/python/initializer.py
+++ src/main/python/initializer.py
@@ -54,6 +54,9 @@ if __name__ == "__main__":
                     #testing dev data
                     features, y, adj_lexicon,all_adj= get_features_y(cwd, dev,False)
                     print(len(features))
+                    print(adj_lexicon)
+                    print(all_adj)
+                    print(len(all_adj))
                     sys.exit(1)
 
                     features, y, adj_lexicon,all_adj= get_features_y(cwd, turkFile,False)
