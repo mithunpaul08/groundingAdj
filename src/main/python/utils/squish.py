1c12d19b934d1ed859c8caec31b4179a7535d5c7
Mithun Paul <mithunpaul@chung.cs.arizona.edu>
2018-01-17 16:44:16 -0700

    runs for 300-20. for 30 epochs

--- src/main/python/utils/squish.py
+++ src/main/python/utils/squish.py
@@ -19,7 +19,7 @@ from tqdm import tqdm
 from utils.grounding import get_features_dev
 torch.manual_seed(1)
 
-hidden_layers=[30,1]
+#hidden_layers=[30,1]
 # no_of_hidden_layers=3
 dense1_size=20
 # dense2_size=10
@@ -74,7 +74,7 @@ class AdjEmb(nn.Module):
 
 
         #the size of the last layer will be the last entry in dense3_size
-        dense3_size=hidden_layers[len(hidden_layers)-1]
+        #dense3_size=hidden_layers[len(hidden_layers)-1]
 
 
         # the layer where you squish the 300 embeddings to a dense layer of whatever size.
@@ -82,35 +82,35 @@ class AdjEmb(nn.Module):
 
         # i.e it takes embeddings as input and returns a dense layer of size 10
         # note: this is also known as the weight vector to be used in an affine
-       # self.linear1 = nn.Linear(self.vec.size(1), dense1_size)
 
+        self.linear1 = nn.Linear(self.vec.size(1), dense1_size)
 
-        #self.tanned=nn.Tanh(self.squish)
+        # self.tanned=nn.Tanh(self.squish)
         # self.linear2 = torch.nn.Linear(dense1_size, dense2_size)
         # self.linear3 = torch.nn.Linear(dense2_size, dense3_size)
 
 
         #dynamically add the hidden layers
-        for index,layer in enumerate(hidden_layers):
-            #first squish alone is from size of embeddings
-            if(index==0):
-                layername="linear"+str(index)
-                self.layername = nn.Linear(self.vec.size(1), hidden_layers[index])
-            else:
-                #second squish onwards dynamically add
-                if((index+1) < len(hidden_layers)):
-                    layername="linear"+str(index)
-                    self.layername = torch.nn.Linear(hidden_layers[index], hidden_layers[index+1])
+        # for index,layer in enumerate(hidden_layers):
+        #     #first squish alone is from size of embeddings
+        #     if(index==0):
+        #         layername="linear"+str(index)
+        #         self.layername = nn.Linear(self.vec.size(1), hidden_layers[index])
+        #     else:
+        #         #second squish onwards dynamically add
+        #         if((index+1) < len(hidden_layers)):
+        #             layername="linear"+str(index)
+        #             self.layername = torch.nn.Linear(hidden_layers[index], hidden_layers[index+1])
 
 
 
 
-
-        print("dense3_size:")
-        print(dense3_size)
+        #
+        # print("dense3_size:")
+        # print(dense3_size)
 
         #the last step: whatever the output of previous layer was concatenate it with the mu and sigma and one-hot vector for turker
-        self.fc = torch.nn.Linear(dense3_size+turkCount+2, 1)
+        self.fc = torch.nn.Linear(dense1_size+turkCount+2, 1)
 
 
 
@@ -137,22 +137,22 @@ class AdjEmb(nn.Module):
         embV=Variable(emb,requires_grad=False)
 
         #
-        # out=F.tanh(self.linear(embV))
+        out=F.tanh(self.linear1(embV))
         # out=F.tanh(self.linear2(out))
         # out=F.tanh(self.linear3(out))
 
 
-        #dynamically add the hidden layers
-        for index,layer in enumerate(hidden_layers):
-            layername="linear"+str(index)
-            #first squish alone is from size of embeddings
-            if(index==0):
-                out=F.tanh(self.layername(embV))
-            else:
-                #second squish onwards dynamically add
-
-                if((index+1) < len(hidden_layers)):
-                    out=F.tanh(self.layername(out))
+        # #dynamically add the hidden layers
+        # for index,layer in enumerate(hidden_layers):
+        #     layername="linear"+str(index)
+        #     #first squish alone is from size of embeddings
+        #     if(index==0):
+        #         out=F.tanh(self.layername(embV))
+        #     else:
+        #         #second squish onwards dynamically add
+        #
+        #         if((index+1) < len(hidden_layers)):
+        #             out=F.tanh(self.layername(out))
 
 
 
