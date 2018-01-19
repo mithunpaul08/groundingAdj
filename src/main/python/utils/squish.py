e22258e75607ae1520c8db97aeb0cd59340c9d8f
Mithun Paul <mithunpaul@chung.cs.arizona.edu>
2018-01-18 17:27:45 -0700

    100-10-1. 10,000 epochs

--- src/main/python/utils/squish.py
+++ src/main/python/utils/squish.py
@@ -22,8 +22,8 @@ torch.manual_seed(1)
 #hidden_layers=[30,1]
 # no_of_hidden_layers=3
 dense1_size=100
-dense2_size=1
-# dense3_size=1
+dense2_size=10
+dense3_size=1
 
 noOfEpochs=10000
 lr=1e-5
@@ -86,7 +86,7 @@ class AdjEmb(nn.Module):
 
         self.linear1 = nn.Linear(self.vec.size(1), dense1_size)
         self.linear2 = torch.nn.Linear(dense1_size, dense2_size)
-        # self.linear3 = torch.nn.Linear(dense2_size, dense3_size)
+        self.linear3 = torch.nn.Linear(dense2_size, dense3_size)
 
 
         #dynamically add the hidden layers
@@ -109,7 +109,7 @@ class AdjEmb(nn.Module):
         # print(dense3_size)
 
         #the last step: whatever the output of previous layer was concatenate it with the mu and sigma and one-hot vector for turker
-        self.fc = torch.nn.Linear(dense2_size+turkCount+2, 1)
+        self.fc = torch.nn.Linear(dense3_size+turkCount+2, 1)
 
 
 
@@ -138,7 +138,7 @@ class AdjEmb(nn.Module):
         #
         out=F.tanh(self.linear1(embV))
         out=F.tanh(self.linear2(out))
-        #out=F.tanh(self.linear3(out))
+        out=F.tanh(self.linear3(out))
 
 
         # #dynamically add the hidden layers
