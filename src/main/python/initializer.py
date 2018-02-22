8328e4a3bd31133da3454b781177ee1649a9aa33
mithun <mithunpaul08@gmail.com>
2018-02-22 12:57:56 -0700

    removed basic commented lines

--- src/main/python/initializer.py
+++ src/main/python/initializer.py
@@ -107,22 +107,13 @@ if __name__ == "__main__":
                 if(myInput=="2"):
 
 
-
-                    #get the embeddings for only the adjectives we need and write it to a file
-                    # cut_glove=cutGlove(adj_lexicon);
-                    # writeDictToFile(cut_glove,cwd,"glove_our_adj")
-                    # sys.exit(1)
-                    # split_entire_data(cwd, entire_turk_data, addTurkerOneHot)
-                    # sys.exit(1)
-
-
                     #run1: run with leave one out cross validationon all the turk experiment data points-i.e no adjective based split
 
                     # read all the data. i.e without training-dev-split. This is for LOOCV
                     features, y, adj_lexicon, all_adj, uniq_turker,uniq_adj_list = get_features_labels_from_data(cwd, entire_turk_data,
                                                                                                                  addAdjOneHot, uniq_turker, addTurkerOneHot)
 
-                     # run1: run with leave one out cross validation
+                    # run1: run with leave one out cross validation
                     #run_nfoldCV_on_turk_data(features, y, adj_lexicon, all_adj,addTurkerOneHot,useEarlyStopping,use4Chunks)
                     run_nfoldCV_on_turk_data_4chunks(features, y, adj_lexicon, all_adj, addTurkerOneHot, useEarlyStopping,
                                              use4Chunks)
@@ -166,26 +157,11 @@ if __name__ == "__main__":
                         adj_lexicon_flipped[idx] = a
 
 
-                    # features, y, adj_lexicon,all_adj=  get_features_dev(cwd, dev,False,uniq_turker)
-                    # print("done reading dev data:")
 
 
                     #############use the trained model to test on test split
 
 
-
-
-
-
-                    # adj_intercepts_learned = learned_weights[:num_adj]
-                    # #pairing weights with adjectives.
-                    # adj_pairs = [(learned_weights[0][i], adj_lexicon_flipped[i]) for i in range(num_adj)]
-
-                    # sorted_adjs = sorted(adj_pairs, key=lambda x: x[0], reverse=True)
-                    #
-                    # #print highest 20 intercepts and lowest 20 intercepts
-                    # print(sorted_adjs[:20])
-                    # print(sorted_adjs[-20:])
                     elapsed_time = time.time() - start_time
                     print("time taken:" + str(elapsed_time/60)+"minutes")
 
@@ -284,40 +260,7 @@ if __name__ == "__main__":
 
                                             print("done loocv for adj based turk data, going to exit")
 
-                                            #
-                                            #
-                                            # features, y, adj_lexicon,all_adj= get_features_y(cwd, turkFile,False)
-                                            # print(features.shape)
-                                            #
-                                            # adj_lexicon_flipped = dict()
-                                            # #total number of unique adjectives
-                                            # num_adj = len(adj_lexicon)
-                                            #
-                                            # #key=index value=adjective
-                                            # for a, idx in adj_lexicon.items():
-                                            #     adj_lexicon_flipped[idx] = a
-                                            #
-                                            # #actual linear regression part- how much weight should it assigne to each of 1-hot-adj-vector, mean and variance
-                                            #
-                                            # #will be of size 1x100=98 adj, one mean and variance
-                                            # learned_weights = runLR(features, y)
-                                            #
-                                            # #print(str(learned_weights.shape))
-                                            # #sys.exit(1)
-                                            # #print("NumUniqueAdj: ", num_adj)
-                                            # # Get the weights that correspond to the individual adjs
-                                            # adj_intercepts_learned = learned_weights[:num_adj]
-                                            # #pairing weights with adjectives.
-                                            # adj_pairs = [(learned_weights[0][i], adj_lexicon_flipped[i]) for i in range(num_adj)]
-                                            #
-                                            # #print(adj_pairs[:2])
-                                            #
-                                            # #sorting them by their weight
-                                            # sorted_adjs = sorted(adj_pairs, key=lambda x: x[0], reverse=True)
-                                            #
-                                            # #print highest 20 intercepts and lowest 20 intercepts
-                                            # print(sorted_adjs[:20])
-                                            # print(sorted_adjs[-20:])
+
                                         else:
 
                                             if(myInput=="5"):
