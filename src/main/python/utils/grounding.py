da3565e2c156f839edb515cd82f44e32fc631951
mithun <mithunpaul08@gmail.com>
2018-02-22 15:04:13 -0700

    removed some printf lines

--- src/main/python/utils/grounding.py
+++ src/main/python/utils/grounding.py
@@ -28,7 +28,7 @@ def predict_grounding(cwd,turkFile):
 
         splitTurk=np.array_split(allIndex,2)
 
-        ###print(df_raw_turk_data["logrespdev"][0])
+
 
         trainingData=splitTurk[0]
         rest=splitTurk[1]
@@ -38,17 +38,13 @@ def predict_grounding(cwd,turkFile):
         dev=dev_test[0]
         test=dev_test[1]
 
-        ###print(trainingData.shape)
-        ###print(dev.shape)
-        ###print(test.shape)
 
-        ###print(trainingData["mean"])
+
         y=np.array([])
         features=np.ndarray(shape=(1,302))
         pathDM = cwd + "/data/" + cbow4
         marneffe_data = loadEmbeddings(pathDM)
-        ##print("done reading embeddings. total number of rows/words in this is:")
-        ##print(len(marneffe_data))
+
         oovCount=0
 
         #For each line in the training part of turk data document:Get the mean and variance from that line in the turk document
@@ -58,46 +54,24 @@ def predict_grounding(cwd,turkFile):
             mean=df_raw_turk_data["mean"][eachTurkRow]
             stddev=df_raw_turk_data["onestdev"][eachTurkRow]
             logRespDev=df_raw_turk_data["logrespdev"][eachTurkRow]
-            ##print("index:"+str(eachTurkRow))
-            ##print("mean"+str(mean))
-            ##print("adjective:"+str(adj))
 
-            ##print("done reading embeddings. total number of rows/words in this is:")
-            ##print(len(marneffe_data))
             if(adj not in marneffe_data):
                 oovCount=oovCount+1
             else:
                 oneoutput=marneffe_data[adj]
-                ##print("shape of oneoutput  is:")
-                ##print((oneoutput.shape))
+
                 withmean=np.append(oneoutput,mean)
                 withstd = np.append(withmean, stddev)
-                ##print(len(withstd))
-                ##print(logRespDev)
-                ##print("size of withstd is:")
-                ##print((withstd.shape))
-                #print("size of y is:")
-                #print((y.shape))
+
                 ylabelLocal=np.array([logRespDev])
                 featuresLocal = np.asarray(withstd)
                 featuresLocal=featuresLocal.transpose()
-                # print("size of featuresLocal is:")
-                # print((featuresLocal.shape))
-                # print("size of ylabelLocal is:")
-                # print((ylabelLocal.shape))
-                # print("size of big features is:")
-                # print((features.shape))
-
-                #print("logrespdev")
                 combinedY=np.append(y,ylabelLocal)
                 combinedFeatures=np.append(features,featuresLocal,axis=0)
                 features=combinedFeatures
                 y=combinedY
 
-                # print("size of big features is:")
-                # print((features.shape))
-                # print("size of big y is:")
-                # print((y.shape))
+
                 sys.exit(1)
 
 
